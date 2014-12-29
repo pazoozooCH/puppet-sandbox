f778c285dc4f70ee49e67c699805f28be8df5289
Martin Meyer <martin.meyer@inftec.ch>
2014-12-29 08:35:56 +0100

    Added servers to hostgroups

--- modules/nagios/manifests/client/default.pp
+++ modules/nagios/manifests/client/default.pp
@@ -12,6 +12,7 @@ define nagios::client::default(
 		alias => $name,
 		address => $address,
 		use => 'generic-host',
+		hostgroups => 'debian-servers,ssh-servers',
 		require => Class['nagios'],
 		notify => Service['nagios3'],
 	}
@@ -22,12 +23,49 @@ define nagios::client::default(
 		require => Nagios_host[ $name ],
 	}
 	
-	nagios_service { $name:
+	# Define common services
+	
+	## Disk Space
+	nagios_service { "${name}_diskSpace":
 		target => "${cfgFileService}",
 		host_name => $name,
 		service_description => 'Disk Space',
 		check_command => 'check_all_disks!20%!10%',
 		use => 'generic-service',
 		require => Nagios_host[$name],
+		notify => Service['nagios3'],
+	}
+	
+	## Current Users
+	nagios_service { "${name}_currentUsers":
+		target => "${cfgFileService}",
+		host_name => $name,
+		service_description => 'Current Users',
+		check_command => 'check_users!20!50',
+		use => 'generic-service',
+		require => Nagios_host[$name],
+		notify => Service['nagios3'],
+	}
+	
+	## Total Processes
+	nagios_service { "${name}_totalProcesses":
+		target => "${cfgFileService}",
+		host_name => $name,
+		service_description => 'Total Processes',
+		check_command => 'check_procs!250!400',
+		use => 'generic-service',
+		require => Nagios_host[$name],
+		notify => Service['nagios3'],
+	}
+	
+	## Current Load
+	nagios_service { "${name}_currentLoad":
+		target => "${cfgFileService}",
+		host_name => $name,
+		service_description => 'Current Load',
+		check_command => 'check_load!5.0!4.0!3.0!10.0!6.0!4.0',
+		use => 'generic-service',
+		require => Nagios_host[$name],
+		notify => Service['nagios3'],
 	}
 }
